763ec76984729ea064354ee4f0603ebdccee37af
ndanglan <nbh381891@gmail.com>
2021-07-02 11:32:12 +0700

    product-list

---
+++ main.js
@@ -0,0 +1,118 @@
+function myFunction(){
+    var x = document.querySelector(".name");
+    var currentVal=x.value;
+
+    if(currentVal == 'serum kosxu'){
+        document.getElementById("demo").innerHTML="Sỉ: 370 - Lẻ: 450";
+    }
+    else if(currentVal == 'mặt nạ online Kosxu' || currentVal == 'mặt nạ kosxu' ){
+        document.getElementById("demo").innerHTML="Sỉ: 550 - Lẻ: 650";
+    }
+    else if(currentVal == 'kem cú kosxu' || currentVal == 'kem cú' ){
+        document.getElementById("demo").innerHTML="Sỉ: 495 - Lẻ: 600";
+    }
+    else if(currentVal == 'nước thần' || currentVal == 'serum kosxu'){
+        document.getElementById("demo").innerHTML="Sỉ: 520 - Lẻ: 620";
+    }
+    else if(currentVal == 'tẩy da chết 2 in 1' || currentVal == 'detox'){
+        document.getElementById("demo").innerHTML="Sỉ: 350 - Lẻ: 450";
+    }
+    else if(currentVal == 'sữa rửa mặt kosxu'){
+        document.getElementById("demo").innerHTML="Sỉ: 330 - Lẻ: 420";
+    }
+    else if(currentVal == 'kem mụn nhím kosxu' || currentVal == 'kem mụn nhím' ){
+        document.getElementById("demo").innerHTML="Sỉ: 490 - Lẻ: 580";
+    }
+    else if(currentVal == 'kem chống nắng kosxu'){
+        document.getElementById("demo").innerHTML="Sỉ: 340 - Lẻ: 420";
+    }
+    else if(currentVal == 'cushion kosxu' ){
+        document.getElementById("demo").innerHTML="Sỉ: 490 - Lẻ: 590";
+    }
+    else if(currentVal == 'dầu tẩy trang kosxu' || currentVal == 'dầu tẩy trang'){
+        document.getElementById("demo").innerHTML="Sỉ: 420 - Lẻ: 520";
+    }
+    else if(currentVal == 'kem body kosxu'){
+        document.getElementById("demo").innerHTML="Sỉ: 400 - Lẻ: 480";
+    }
+    else if(currentVal == 'set tẩy trắng kosxu'){
+        document.getElementById("demo").innerHTML="Sỉ: 950 - Lẻ: 1100";
+    }
+    else if(currentVal == 'tẩy da chết body kosxu' || currentVal == 'tẩy da chết body'){
+        document.getElementById("demo").innerHTML="Sỉ: 360 - Lẻ: 450";
+    }
+    else if(currentVal == 'kem winter kosxu' || currentVal == 'kem winter'){
+        document.getElementById("demo").innerHTML="Sỉ: 550 - Lẻ: 650";
+    }
+    else if(currentVal == 'lotion kosxu'){
+        document.getElementById("demo").innerHTML="Sỉ: 310 - Lẻ: 390";
+    }
+    else if(currentVal == 'son kosxu' || currentVal == 'son dưỡng'){
+        document.getElementById("demo").innerHTML="Sỉ: 220 - Lẻ: 290";
+    }
+    else if(currentVal == 'phấn phủ kosxu' || currentVal == 'phấn phủ'){
+        document.getElementById("demo").innerHTML="Sỉ: 400 - Lẻ: 490";
+    }
+    else if(currentVal == 'kem phủ tuyết kosxu' || currentVal == 'kem phủ tuyết'){
+        document.getElementById("demo").innerHTML="Sỉ: 850 - Lẻ: 990";
+    }
+    else if(currentVal == 'amploule mắt kosxu' || currentVal == 'amploule'|| currentVal == 'amploule mắt' || currentVal == 'amploule kosxu'){
+        document.getElementById("demo").innerHTML="Sỉ: 520 - Lẻ: 620";
+    }
+    else if(currentVal == 'tế bào gốc' || currentVal == 'tế bào gốc kosxu'){
+        document.getElementById("demo").innerHTML="Sỉ: 770 - Lẻ: 890";
+    }
+    else if(currentVal == '4.0' || currentVal == 'serum lên men 4.0' || currentVal == 'serum lên men'){
+        document.getElementById("demo").innerHTML="Sỉ: 480 - Lẻ: 580";
+    }
+    else if(currentVal == 'son thỏi lì kosxu' || currentVal == 'son thỏi lì'){
+        document.getElementById("demo").innerHTML="Sỉ: 230 - Lẻ: 320";
+    }
+
+    // The-lab
+    else if(currentVal == 'set blancdoux ngày' || currentVal == 'set blancdoux' || currentVal == 'blancdoux'){
+        document.getElementById("demo").innerHTML="Sỉ: 650 - Lẻ: 750";
+    }
+    else if(currentVal == 'mặt nạ ngủ blanc' || currentVal == 'mặt nạ ngủ'){
+        document.getElementById("demo").innerHTML="Sỉ: 470 - Lẻ: 550";
+    }
+    else if(currentVal == 'mặt nạ lột blanc' || currentVal == 'mặt nạ lột'){
+        document.getElementById("demo").innerHTML="Sỉ: 470 - Lẻ: 570";
+    }
+    else if(currentVal == 'toner blanc cánh hoa' || currentVal == 'toner cánh hoa' || currentVal == 'essence blanc'){
+        document.getElementById("demo").innerHTML="Sỉ: 550 - Lẻ: 650";
+    }
+    else if(currentVal == 'essence blanc cánh hoa' || currentVal == 'essence cánh hoa'){
+        document.getElementById("demo").innerHTML="Sỉ: 630 - Lẻ: 750";
+    }
+    else if(currentVal == 'bông lau mụn thelab' || currentVal == 'bông lau mụn'){
+        document.getElementById("demo").innerHTML="Sỉ: 430 - Lẻ: 525";
+    }
+    else if(currentVal == 'toner thelab'){
+        document.getElementById("demo").innerHTML="Sỉ: 450 - Lẻ: 550";
+    }
+    else if(currentVal == 'amploule thelab'){
+        document.getElementById("demo").innerHTML="Sỉ: 400 - Lẻ: 500";
+    }
+    else if(currentVal == 'kem ẩm thelab' || currentVal == 'kem ẩm'){
+        document.getElementById("demo").innerHTML="Sỉ: 370 - Lẻ: 450";
+    }
+    else if(currentVal == 'sữa rửa mặt thelab'){
+        document.getElementById("demo").innerHTML="Sỉ: 300 - Lẻ: 395";
+    }
+    else if(currentVal == 'mask thelab' || currentVal == 'mặt nạ thelab'){
+        document.getElementById("demo").innerHTML="Sỉ: 400 - Lẻ: 480";
+    }
+    else if(currentVal == 'lotion thelab'){
+        document.getElementById("demo").innerHTML="Sỉ: 360 - Lẻ: 450";
+    }
+    else if(currentVal == 'kem chống nắng thelab'){
+        document.getElementById("demo").innerHTML="Sỉ: 305 - Lẻ: 375";
+    }
+    else if(currentVal == 'set5 viên tinh chất thelab' || currentVal == 'tinh chất thelab'|| currentVal == 'set5 viên tinh chất'){
+        document.getElementById("demo").innerHTML="Sỉ: 330 - Lẻ: 420";
+    }
+    else if(currentVal == 'cushion thelab'){
+        document.getElementById("demo").innerHTML="Sỉ: 480 - Lẻ: 575";
+    }
+}
