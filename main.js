40223f2e4d10c7594e1b772fbdefa351bac2e6ec
ndanglan <nbh381891@gmail.com>
2021-07-02 11:47:55 +0700

    sua-lan2

--- main.js
+++ main.js
@@ -2,117 +2,117 @@ function myFunction(){
     var x = document.querySelector(".name");
     var currentVal=x.value;
 
-    if(currentVal == 'serum kosxu'){
+    if(currentVal == 'Serum kosxu'){
         document.getElementById("demo").innerHTML="Sỉ: 370 - Lẻ: 450";
     }
-    else if(currentVal == 'mặt nạ online Kosxu' || currentVal == 'mặt nạ kosxu' ){
+    else if(currentVal == 'Mặt nạ online Kosxu' || currentVal == 'Mặt nạ kosxu' ){
         document.getElementById("demo").innerHTML="Sỉ: 550 - Lẻ: 650";
     }
-    else if(currentVal == 'kem cú kosxu' || currentVal == 'kem cú' ){
+    else if(currentVal == 'Kem cú kosxu' || currentVal == 'Kem cú' ){
         document.getElementById("demo").innerHTML="Sỉ: 495 - Lẻ: 600";
     }
-    else if(currentVal == 'nước thần' || currentVal == 'serum kosxu'){
+    else if(currentVal == 'Nước thần'){
         document.getElementById("demo").innerHTML="Sỉ: 520 - Lẻ: 620";
     }
-    else if(currentVal == 'tẩy da chết 2 in 1' || currentVal == 'detox'){
+    else if(currentVal == 'Tẩy da chết 2 in 1' || currentVal == 'Detox'){
         document.getElementById("demo").innerHTML="Sỉ: 350 - Lẻ: 450";
     }
-    else if(currentVal == 'sữa rửa mặt kosxu'){
+    else if(currentVal == 'Sữa rửa mặt kosxu'){
         document.getElementById("demo").innerHTML="Sỉ: 330 - Lẻ: 420";
     }
-    else if(currentVal == 'kem mụn nhím kosxu' || currentVal == 'kem mụn nhím' ){
+    else if(currentVal == 'Kem mụn nhím kosxu' || currentVal == 'Kem mụn nhím' ){
         document.getElementById("demo").innerHTML="Sỉ: 490 - Lẻ: 580";
     }
-    else if(currentVal == 'kem chống nắng kosxu'){
+    else if(currentVal == 'Kem chống nắng kosxu'){
         document.getElementById("demo").innerHTML="Sỉ: 340 - Lẻ: 420";
     }
-    else if(currentVal == 'cushion kosxu' ){
+    else if(currentVal == 'Cushion kosxu' ){
         document.getElementById("demo").innerHTML="Sỉ: 490 - Lẻ: 590";
     }
-    else if(currentVal == 'dầu tẩy trang kosxu' || currentVal == 'dầu tẩy trang'){
+    else if(currentVal == 'Dầu tẩy trang kosxu' || currentVal == 'Dầu tẩy trang'|| currentVal == 'Tẩy trang'){
         document.getElementById("demo").innerHTML="Sỉ: 420 - Lẻ: 520";
     }
-    else if(currentVal == 'kem body kosxu'){
+    else if(currentVal == 'Kem body kosxu'){
         document.getElementById("demo").innerHTML="Sỉ: 400 - Lẻ: 480";
     }
-    else if(currentVal == 'set tẩy trắng kosxu'){
+    else if(currentVal == 'Set tẩy trắng kosxu'){
         document.getElementById("demo").innerHTML="Sỉ: 950 - Lẻ: 1100";
     }
-    else if(currentVal == 'tẩy da chết body kosxu' || currentVal == 'tẩy da chết body'){
+    else if(currentVal == 'Tẩy da chết body kosxu' || currentVal == 'Tẩy da chết body'){
         document.getElementById("demo").innerHTML="Sỉ: 360 - Lẻ: 450";
     }
-    else if(currentVal == 'kem winter kosxu' || currentVal == 'kem winter'){
+    else if(currentVal == 'Kem winter kosxu' || currentVal == 'Kem winter'){
         document.getElementById("demo").innerHTML="Sỉ: 550 - Lẻ: 650";
     }
-    else if(currentVal == 'lotion kosxu'){
+    else if(currentVal == 'Lotion kosxu'){
         document.getElementById("demo").innerHTML="Sỉ: 310 - Lẻ: 390";
     }
-    else if(currentVal == 'son kosxu' || currentVal == 'son dưỡng'){
+    else if(currentVal == 'Son kosxu' || currentVal == 'Son dưỡng'){
         document.getElementById("demo").innerHTML="Sỉ: 220 - Lẻ: 290";
     }
-    else if(currentVal == 'phấn phủ kosxu' || currentVal == 'phấn phủ'){
+    else if(currentVal == 'Phấn phủ kosxu' || currentVal == 'Phấn phủ'){
         document.getElementById("demo").innerHTML="Sỉ: 400 - Lẻ: 490";
     }
-    else if(currentVal == 'kem phủ tuyết kosxu' || currentVal == 'kem phủ tuyết'){
+    else if(currentVal == 'Kem phủ tuyết kosxu' || currentVal == 'Kem phủ tuyết'){
         document.getElementById("demo").innerHTML="Sỉ: 850 - Lẻ: 990";
     }
-    else if(currentVal == 'amploule mắt kosxu' || currentVal == 'amploule'|| currentVal == 'amploule mắt' || currentVal == 'amploule kosxu'){
+    else if(currentVal == 'Amploule mắt kosxu' || currentVal == 'Amploule'|| currentVal == 'Amploule mắt' || currentVal == 'Amploule kosxu'|| currentVal == 'Mắt'){
         document.getElementById("demo").innerHTML="Sỉ: 520 - Lẻ: 620";
     }
-    else if(currentVal == 'tế bào gốc' || currentVal == 'tế bào gốc kosxu'){
+    else if(currentVal == 'Tế bào gốc' || currentVal == 'Tế bào gốc kosxu'){
         document.getElementById("demo").innerHTML="Sỉ: 770 - Lẻ: 890";
     }
-    else if(currentVal == '4.0' || currentVal == 'serum lên men 4.0' || currentVal == 'serum lên men'){
+    else if(currentVal == '4.0' || currentVal == 'Serum lên men 4.0' || currentVal == 'Serum lên men'){
         document.getElementById("demo").innerHTML="Sỉ: 480 - Lẻ: 580";
     }
-    else if(currentVal == 'son thỏi lì kosxu' || currentVal == 'son thỏi lì'){
+    else if(currentVal == 'Son thỏi lì kosxu' || currentVal == 'Son thỏi lì'){
         document.getElementById("demo").innerHTML="Sỉ: 230 - Lẻ: 320";
     }
 
     // The-lab
-    else if(currentVal == 'set blancdoux ngày' || currentVal == 'set blancdoux' || currentVal == 'blancdoux'){
+    else if(currentVal == 'Set blancdoux ngày' || currentVal == 'Set blancdoux' || currentVal == 'Blancdoux'){
         document.getElementById("demo").innerHTML="Sỉ: 650 - Lẻ: 750";
     }
-    else if(currentVal == 'mặt nạ ngủ blanc' || currentVal == 'mặt nạ ngủ'){
+    else if(currentVal == 'Mặt nạ ngủ blanc' || currentVal == 'Mặt nạ ngủ'){
         document.getElementById("demo").innerHTML="Sỉ: 470 - Lẻ: 550";
     }
-    else if(currentVal == 'mặt nạ lột blanc' || currentVal == 'mặt nạ lột'){
+    else if(currentVal == 'Mặt nạ lột blanc' || currentVal == 'Mặt nạ lột'){
         document.getElementById("demo").innerHTML="Sỉ: 470 - Lẻ: 570";
     }
-    else if(currentVal == 'toner blanc cánh hoa' || currentVal == 'toner cánh hoa' || currentVal == 'essence blanc'){
+    else if(currentVal == 'Toner blanc cánh hoa' || currentVal == 'Toner cánh hoa' || currentVal == 'Essence blanc'){
         document.getElementById("demo").innerHTML="Sỉ: 550 - Lẻ: 650";
     }
-    else if(currentVal == 'essence blanc cánh hoa' || currentVal == 'essence cánh hoa'){
+    else if(currentVal == 'Essence blanc cánh hoa' || currentVal == 'Essence cánh hoa'){
         document.getElementById("demo").innerHTML="Sỉ: 630 - Lẻ: 750";
     }
-    else if(currentVal == 'bông lau mụn thelab' || currentVal == 'bông lau mụn'){
+    else if(currentVal == 'Bông lau mụn thelab' || currentVal == 'Bông lau mụn'){
         document.getElementById("demo").innerHTML="Sỉ: 430 - Lẻ: 525";
     }
-    else if(currentVal == 'toner thelab'){
+    else if(currentVal == 'Toner thelab'){
         document.getElementById("demo").innerHTML="Sỉ: 450 - Lẻ: 550";
     }
-    else if(currentVal == 'amploule thelab'){
+    else if(currentVal == 'Amploule thelab'){
         document.getElementById("demo").innerHTML="Sỉ: 400 - Lẻ: 500";
     }
-    else if(currentVal == 'kem ẩm thelab' || currentVal == 'kem ẩm'){
+    else if(currentVal == 'Kem ẩm thelab' || currentVal == 'Kem ẩm'){
         document.getElementById("demo").innerHTML="Sỉ: 370 - Lẻ: 450";
     }
-    else if(currentVal == 'sữa rửa mặt thelab'){
+    else if(currentVal == 'Sữa rửa mặt thelab'){
         document.getElementById("demo").innerHTML="Sỉ: 300 - Lẻ: 395";
     }
-    else if(currentVal == 'mask thelab' || currentVal == 'mặt nạ thelab'){
+    else if(currentVal == 'Mask thelab' || currentVal == 'Mặt nạ thelab'){
         document.getElementById("demo").innerHTML="Sỉ: 400 - Lẻ: 480";
     }
-    else if(currentVal == 'lotion thelab'){
+    else if(currentVal == 'Lotion thelab'){
         document.getElementById("demo").innerHTML="Sỉ: 360 - Lẻ: 450";
     }
-    else if(currentVal == 'kem chống nắng thelab'){
+    else if(currentVal == 'Kem chống nắng thelab'){
         document.getElementById("demo").innerHTML="Sỉ: 305 - Lẻ: 375";
     }
-    else if(currentVal == 'set5 viên tinh chất thelab' || currentVal == 'tinh chất thelab'|| currentVal == 'set5 viên tinh chất'){
+    else if(currentVal == 'Set5 viên tinh chất thelab' || currentVal == 'Tinh chất thelab'|| currentVal == 'Set5 viên tinh chất'){
         document.getElementById("demo").innerHTML="Sỉ: 330 - Lẻ: 420";
     }
-    else if(currentVal == 'cushion thelab'){
+    else if(currentVal == 'Cushion thelab'){
         document.getElementById("demo").innerHTML="Sỉ: 480 - Lẻ: 575";
     }
 }
